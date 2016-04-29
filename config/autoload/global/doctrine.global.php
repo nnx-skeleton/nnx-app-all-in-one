233ff7a6cd8ba534e7da96c42abc2916c120a1e5
dmitry <plitakov@platformasoft.ru>
2016-04-29 13:20:56 +0300

    add config files for doctrine

---
+++ config/autoload/global/doctrine.global.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'doctrine' => [
+        'configuration' => [
+            'orm_default' => [
+                'metadata_cache' => 'memcached'
+            ]
+        ],
+
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOPgSql\Driver',
+                'params' => [
+
+                ]
+            ],
+        ],
+        'driver' => [
+            'orm_default' => [
+                'drivers' => [
+                    'Nnx\Member\Organization\Entity\Member' => 'NnxMemberOrganizationEntityMember',
+                    'Nnx\Member\Organization\Entity\Organization' => 'NnxMemberOrganizationEntityOrganization',
+                    'Nnx\Member\Organization\Entity\User' => 'NnxMemberOrganizationEntityUser',
+
+                    'Nnx\Member\User\Entity\User' => 'NnxMemberUserEntityUser'
+
+                ],
+            ],
+        ],
+    ],
+];
---
+++ config/autoload/local/doctrine.local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'params' => [
+                    'host' => '127.0.0.1',
+                    'port' => '5432',
+                    'user' => 'developer',
+                    'password' => 'developer',
+                    'dbname' => 'nnx',
+                ]
+            ],
+        ],
+    ],
+];
