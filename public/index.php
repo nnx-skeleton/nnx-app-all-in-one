db04d664f534ae248049c44e300ba548c9fe35b0
nnx-skelet <nnx-skeleton@yandex.ru>
2016-05-04 12:24:42 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 chdir(dirname(__DIR__));
 
-if (php_sapi_name() === 'cli-server') {
+if (php_sapi_name() === 'cli-server'){
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
     if (__FILE__ !== $path && is_file($path)) {
         return false;
